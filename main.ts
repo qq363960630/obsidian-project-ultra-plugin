ee111c88d1989aca7d01f5f873f862082558b62c
liuguo <363960630@qq.com>
2024-04-30 16:59:36 +0800

    add ftp tools

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
+import { CreateProjectView } from 'ui/CreateProject';
+
+import { FtpMain } from 'ui/FtpMain';
+
+import { CreatePassword } from 'ui/CreatePassword';
+
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
@@ -17,9 +23,9 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('dice', 'amytools', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new Notice('Project Plugin of Obsidian');
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
@@ -28,23 +34,33 @@ export default class MyPlugin extends Plugin {
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
+		// This adds a create command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'project-ultra-create',
+			name: 'Create project',
 			callback: () => {
-				new SampleModal(this.app).open();
+				new CreateProjectView(this.app).open();
 			}
 		});
+
+
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+			id: 'open-ftp',
+			name: 'Open Ftp',
+			callback: () => {
+				new FtpMain(this.app).open();
 			}
 		});
+
+		this.addCommand({
+			id: 'create-password',
+			name: 'Create a random password',
+			callback: () => {
+				new CreatePassword(this.app).open();
+			}
+		});
+
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
 			id: 'open-sample-modal-complex',
@@ -97,13 +113,54 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		let { contentEl } = this;
+		contentEl.createEl('h1', { text: 'Add New Project' });
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
+	}
+}
+
+class CreateModal extends Modal {
+	constructor(app: App) {
+		super(app);
+	}
+
+	onOpen() {
+		let { contentEl } = this;
+		contentEl.createEl('h1', { text: 'Add New Project' });
+		let form = contentEl.createEl('form');
+
+		let projectNameInput = form.createEl('p')
+			.createDiv('1')
+			.createEl('input', {
+				type: 'text',
+				placeholder: 'Project Name'
+			});
+
+		let projectDescInput = form.createEl('p')
+			.createDiv('1')
+			.createEl('input', {
+				type: 'text',
+				placeholder: 'Project Description'
+			});
+
+		form.createEl('button', { text: 'Save Project' }).addEventListener('click', (e) => {
+			e.preventDefault();
+			this.saveProject(projectNameInput.value, projectDescInput.value);
+		});
+	}
+
+	onClose() {
+		const { contentEl } = this;
+		contentEl.empty();
+	}
+
+	saveProject(name: string, description: string) {
+		// 实现保存逻辑
+		console.log('Project Saved:', name, description);
 	}
 }
 
@@ -116,7 +173,7 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
